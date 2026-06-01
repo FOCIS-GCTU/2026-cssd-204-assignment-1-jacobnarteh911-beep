30fb312633f07990be93de42377688dcc34def1d
jacobnarteh911-beep <jacobnarteh911@gmail.com>
2026-06-01 17:32:48 +0200

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Narh John Teye
+# STUDENT ID:  2425404765
+# Email: nobody7516@gmail.com
+# Grader: Emma
+#
+# On my honor, Narh John Teye, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials JTN in stylized large block letters made of the letters J, T, and N
 
+    print()
+    print("...JTN")
+    print()
+    print("...JJJJJJJJJJJJ****...TTTTTTTTTTTT****...NN........NN****...")
+    print(".......JJJJ....****.......TTTT....****...NNN.......NN****...")
+    print(".......JJJJ....****......TTTT....****...NNNN......NN****...")
+    print(".......JJJJ....****......TTTT....****...NN.NN.....NN****...")
+    print(".......JJJJ....****......TTTT....****...NN..NN....NN****...")
+    print(".......JJJJ....****......TTTT....****...NN...NN...NN****...")
+    print("...JJJ..JJJJ...****......TTTT....****...NN....NN..NN****...")
+    print("...JJJ..JJJJ...****......TTTT....****...NN.....NNNNN****...")
+    print("...JJJJJJJJJJ..****......TTTT....****...NN......NNNN****...")
+    print(".....JJJJJJJJ..***......TTTT....****...NN.......NNN****...")
+    print()
 
 
 main()
